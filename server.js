6eec9e2c82a5d664f1e074124351753bda2e13d0
lucacasa1986 <lucacasa1986gmail.com>
2018-03-19 20:23:51 +0000

    proxy

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 //Install express server
 const express = require('express');
 const app = express();
-var proxy = require('express-http-proxy');
+var proxy = require('http-proxy-middleware');
 
- 
-app.use('/api', proxy('https://lucacasa1986.pythonanywhere.com/api'));
-// Serve only the static files form the dist directory
+ // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
+app.use('/api', proxy({target: 'https://lucacasa1986.pythonanywhere.com', changeOrigin: true}));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
