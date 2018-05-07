5e8ecf74e4cb684159ffda3d67e744998ae60008
Luca Casarotti <luca.casarotti@dedalus.eu>
2018-05-07 12:15:32 +0200

    tooltip sempre visibili su grafico a torta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TagService } from './tag.service';
 import { MovimentoServiceService } from './movimenti/movimento-service.service';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
 
   constructor( private _tagService: TagService, private _movimentoService: MovimentoServiceService)
   {
     _tagService.init();
     _movimentoService.init();
+  }
+
+  ngOnInit() {
+    Chart.pluginService.register({
+			beforeRender: function (chart) {
+				if (chart.config.options.showAllTooltips) {
+					// create an array of tooltips
+					// we can't use the chart tooltip because there is only one tooltip per chart
+					chart.pluginTooltips = [];
+					chart.config.data.datasets.forEach(function (dataset, i) {
+						chart.getDatasetMeta(i).data.forEach(function (sector, j) {
+							chart.pluginTooltips.push(new Chart.Tooltip({
+								_chart: chart.chart,
+								_chartInstance: chart,
+								_data: chart.data,
+								_options: chart.options.tooltips,
+								_active: [sector]
+							}, chart));
+						});
+					});
+
+					// turn off normal tooltips
+					chart.options.tooltips.enabled = false;
+				}
+			},
+			afterDraw: function (chart, easing) {
+				if (chart.config.options.showAllTooltips) {
+					// we don't want the permanent tooltips to animate, so don't do anything till the animation runs atleast once
+					if (!chart.allTooltipsOnce) {
+						if (easing !== 1)
+							return;
+						chart.allTooltipsOnce = true;
+					}
+
+					// turn on tooltips
+					chart.options.tooltips.enabled = true;
+					Chart.helpers.each(chart.pluginTooltips, function (tooltip) {
+						tooltip.initialize();
+						tooltip.update();
+						// we don't actually need this since we are not animating tooltips
+						tooltip.pivot();
+						tooltip.transition(easing).draw();
+					});
+					chart.options.tooltips.enabled = false;
+				}
+			}
+		})
   }
 }
--- src/app/movimenti/grafico/grafico.component.ts
+++ src/app/movimenti/grafico/grafico.component.ts
@@ -63,6 +63,7 @@ export class GraficoComponent implements OnChanges, AfterViewInit {
           ]
         },
         options: {
+          showAllTooltips: true,
           legend: {
             display: false
          },
